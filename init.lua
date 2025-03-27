db54353ece96bece51a389334602038288da3b1f
Khairizal-Bin-Khalid <khairizal.khalid@maybank.com>
2025-03-27 18:07:23 +0800

    add toggle to disable conform.

--- init.lua
+++ init.lua
@@ -688,6 +688,21 @@ require('lazy').setup({
     cmd = { 'ConformInfo' },
     keys = {
       {
+        '<leader>tf',
+        function()
+          -- If autoformat is currently disabled for this buffer,
+          -- then enable it, otherwise disable it
+          if vim.b.disable_autoformat then
+            vim.cmd 'FormatEnable'
+            vim.notify 'Enabled autoformat for current buffer'
+          else
+            vim.cmd 'FormatDisable!'
+            vim.notify 'Disabled autoformat for current buffer'
+          end
+        end,
+        desc = 'Toggle autoformat for current buffer',
+      },
+      {
         '<leader>f',
         function()
           require('conform').format { async = true, lsp_format = 'fallback' }
@@ -699,19 +714,13 @@ require('lazy').setup({
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        local lsp_format_opt
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          lsp_format_opt = 'never'
-        else
-          lsp_format_opt = 'fallback'
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
         end
+        local disable_filetypes = { c = false, cpp = false }
         return {
           timeout_ms = 500,
-          lsp_format = lsp_format_opt,
+          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
         }
       end,
       formatters_by_ft = {
@@ -726,6 +735,7 @@ require('lazy').setup({
         typescript = { 'prettier', stop_after_first = true },
         javascriptreact = { 'prettier', stop_after_first = true },
         java = { 'javaformatter' },
+        typescriptreact = { 'prettier', stop_after_first = true },
       },
       formatters = {
         javaformatter = {
@@ -740,6 +750,29 @@ require('lazy').setup({
         },
       },
     },
+    config = function(_, opts)
+      require('conform').setup(opts)
+
+      vim.api.nvim_create_user_command('FormatDisable', function(args)
+        if args.bang then
+          -- :FormatDisable! disables autoformat for this buffer only
+          vim.b.disable_autoformat = true
+        else
+          -- :FormatDisable disables autoformat globally
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = 'Disable autoformat-on-save',
+        bang = true, -- allows the ! variant
+      })
+
+      vim.api.nvim_create_user_command('FormatEnable', function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = 'Re-enable autoformat-on-save',
+      })
+    end,
   },
 
   { -- Autocompletion
