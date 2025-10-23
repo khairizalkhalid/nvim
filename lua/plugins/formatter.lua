434df00afbf2ef0b31bb7d765053009b31e36f7b
khairizalkhalid <khairizalkhalid@outlook.com>
2025-10-23 11:55:15 +0800

    feat: add conform.nvim with manual format and toggle format-on-save

---
+++ lua/plugins/formatter.lua
@@ -0,0 +1,67 @@
+-- Formatting with conform.nvim
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>f",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      mode = "",
+      desc = "[F]ormat buffer",
+    },
+    {
+      "<leader>tf",
+      function()
+        if vim.b.disable_autoformat or vim.g.disable_autoformat then
+          vim.b.disable_autoformat = false
+          vim.g.disable_autoformat = false
+          print("Format on save: enabled")
+        else
+          vim.b.disable_autoformat = true
+          print("Format on save: disabled")
+        end
+      end,
+      desc = "[T]oggle [F]ormat on save",
+    },
+  },
+  opts = {
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      -- Disable format on save if toggled off
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      return {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      }
+    end,
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
+      json = { "prettier" },
+      yaml = { "prettier" },
+      markdown = { "prettier" },
+      go = { "goimports", "gofmt" },
+      rust = { "rustfmt" },
+      -- Add more formatters as needed
+    },
+  },
+}
+
+-- USAGE:
+-- <leader>f - Format current buffer manually
+-- <leader>tf - Toggle format on save (enabled by default)
+--
+-- To install formatters, use Mason:
+-- :Mason
+-- Then search for formatters like: stylua, prettier, black, etc.
